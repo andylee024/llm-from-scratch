0751abee231b7b870bbe7ea1ea5c888f2bddae25
Andy Lee <andylee024@gmail.com>
2025-03-03 15:29:16 +0800

    Add tokenizer V1

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,10 +1,18 @@
 import re
 
+SPECIAL_CHARACTERS = r'([,.:;?_!"()\']|--|\s)'
+
+
+def raw_text_to_list_of_words(text):
+    words = re.split(SPECIAL_CHARACTERS, text)
+    words = [w.strip() for w in words if w.strip()]
+    return words
+
+
 class VocabularyBuilder:
     """Responsible for building vocabulary based on a list of text sources"""
 
     def __init__(self):
-        self.special_characters = r'([,.:;?_!"()\']|--|\s)'
         self.vocabulary = []
 
     def _add_to_vocabulary(self, words):
@@ -12,8 +20,7 @@ class VocabularyBuilder:
         self.vocabulary = list(set(self.vocabulary))
 
     def _build_vocabulary_for_single_text(self, text):
-        words = re.split(self.special_characters, text)
-        words = [w.strip() for w in words if w.strip()]
+        words = raw_text_to_list_of_words(text)
         words = sorted(set(words))
         self._add_to_vocabulary(words)
             
@@ -27,16 +34,17 @@ class VocabularyBuilder:
 
 class SimpleTokenizerV1:
     def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i:s for s,i in vocab.items()}
-    
+        self.str_to_int = None
+        self.int_to_str = None
+        self._tokenize_vocabulary(vocab)
+
+    def _tokenize_vocabulary(self, vocabulary):
+        self.int_to_str = {i: s for (i,s) in enumerate(vocabulary)}
+        self.str_to_int = {s: i for (i,s) in enumerate(vocabulary)}
 
     def encode(self, text):
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
-        ]
-        ids = [self.str_to_int[s] for s in preprocessed]
+        words = raw_text_to_list_of_words(text)
+        ids = [self.str_to_int[s] for s in words]
         return ids
 
     def decode(self, ids):
@@ -46,11 +54,23 @@ class SimpleTokenizerV1:
 
 
 def test():
+
+    # test vocabulary builder
     text_sources = ["/Users/andylee/Projects/llm-from-scratch/data/the-verdict.txt"]
     vb = VocabularyBuilder()
     vb.build_vocabulary(text_sources)
 
-    print(sorted(vb.vocabulary))
+    # test tokenizer
+    tk = SimpleTokenizerV1(vb.vocabulary)
+    path = "/Users/andylee/Projects/llm-from-scratch/data/the-verdict.txt"
+    with open(path, "r", encoding="utf-8") as f:
+        raw_text = f.read()
+        raw_text = raw_text[:400]
+        print("raw_text : ", raw_text)
+        enocded = tk.encode(raw_text)
+        print("tokenized: ", tk.encode(raw_text))
+        print("characters_decoded " , tk.decode(enocded))
+
 
 if __name__ == "__main__":
     test()
